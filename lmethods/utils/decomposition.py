6594c5e14cfad66faf204687a7aa01461d4fc9fa
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2024-07-08 14:08:26 +0300

    feat(recursive_prompting): search strategies

--- lmethods/utils/decomposition.py
+++ lmethods/utils/decomposition.py
@@ -12,3 +12,13 @@ class DependencySyntax(str, Enum):
     The id of the sub-problem within brackets, followed by the comma-separated ids of the sub-problems it depends on within parenthesis.
     E.g., "[P3](P0, P2)".
     """
+
+
+class SearchStrategy(str, Enum):
+    """The strategy used to traverse a decomposition graph."""
+
+    DFS = "depth_first"
+    """The sub-problems are searched for in a depth-first order."""
+
+    BFS = "breadth_first"
+    """The sub-problems are searched for in a breadth-first order."""
